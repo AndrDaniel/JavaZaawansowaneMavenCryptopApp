4e79e99a8a9a60afeb35d6c6b53427c665695e19
Daanko24 <danielandrosiuk@wp.pl>
2020-04-25 15:12:04 +0200

    Gui view add some function

--- Rot13MAven/src/main/java/sda/cipher/GUI/MainFrame.java
+++ Rot13MAven/src/main/java/sda/cipher/GUI/MainFrame.java
@@ -1,85 +1,160 @@
 package sda.cipher.GUI;
 
+import tool.InputManager;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements ActionListener {
+    private JTextArea logArea;
     public void startGui() {
         JFrame mainFrame = new JFrame("Crypto app");
         mainFrame.setMinimumSize(new Dimension(400, 200));
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        mainFrame.setJMenuBar(addMenuBar());
         mainFrame.add(addMainPanel());
         mainFrame.setVisible(true);
         mainFrame.pack();
     }
+
+    private JMenuBar addMenuBar() {
+        JMenu fileMenu = new JMenu("File");
+        JMenu optMenu = new JMenu("Options");
+
+        JMenuItem exit = new JMenuItem("Exit");
+        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.ALT_MASK));
+//        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4,ActionEvent.ALT_MASK));
+        exit.addActionListener(this);
+        fileMenu.add(exit);
+
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(fileMenu);
+        menuBar.add(optMenu);
+
+
+        return menuBar;
+
+    }
+
     private Component addMainPanel() {
         JPanel panel = new JPanel();
         GridBagLayout layout = new GridBagLayout();
         panel.setLayout(layout);
         GridBagConstraints constr = new GridBagConstraints();
+        logArea = new JTextArea(8, 10);
+
         //TODO get this value from properties
         JLabel titleLabel = new JLabel("SDA Enigma Machine");
         titleLabel.setFont(new Font("Arial", Font.BOLD, 28));
-        titleLabel.setForeground(Color.RED);
+        titleLabel.setForeground(Color.BLUE);
+
+
         constr.fill = GridBagConstraints.CENTER;
         constr.gridx = 0;
         constr.gridy = 0;
         panel.add(titleLabel, constr);
+
         constr.fill = GridBagConstraints.HORIZONTAL;
         constr.gridy = 1;
         panel.add(addSubPanelWithControllers(), constr);
-        JButton runAlgorithmBtn = new JButton("Run algorithm");
+
+        JButton runAlgorithmBtn = new JButton("Run and let miracles happen");
         constr.gridy = 2;
-        constr.ipadx = 2;
-        constr.ipady = 2;
+
+        constr.insets = new Insets(2, 2, 2, 2);
         panel.add(runAlgorithmBtn, constr);
-        JTextArea logArea = new JTextArea(8, 10);
+
+
+        logArea.setLineWrap(true);
         constr.gridy = 3;
-        panel.add(logArea, constr);
+        JScrollPane scroll = new JScrollPane(logArea);
+//        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scroll.setVerticalScrollBarPolicy((JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED));
+        panel.add(scroll, constr);
+
         return panel;
+
     }
+
     private Component addSubPanelWithControllers() {
+
         JPanel subPanel = new JPanel(new GridBagLayout());
+
         GridBagConstraints constraints = new GridBagConstraints();
+
         JLabel sourceLabel = new JLabel("Source file:");
+
         JLabel destinationLabel = new JLabel("Destination:");
+
         JLabel chooseLabel = new JLabel("Choose cipher");
+
         JTextField inputField = new JTextField("input.txt", 10);
+
         JTextField outputField = new JTextField("output.txt", 10);
+
         JButton choseInput = new JButton("Select");
+        choseInput.addActionListener(new InputManager(inputField,logArea));
         JButton choseOutput = new JButton("Select");
+        choseOutput.addActionListener(new InputManager(outputField,logArea));
+
         JComboBox ciphersCombo = new JComboBox(new String[]{"Cesar", "ROT-13"});
+
         JRadioButton encodeRadioBtn = new JRadioButton("Encode");
+
         encodeRadioBtn.setSelected(true);
+
         JRadioButton decodeRadioBtn = new JRadioButton("Decode");
+
         ButtonGroup group = new ButtonGroup();
+
         group.add(encodeRadioBtn);
+
         group.add(decodeRadioBtn);
+
         constraints.insets = new Insets(2, 2, 2, 2);
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.gridy = 0;
         constraints.gridx = 0;
+
         subPanel.add(sourceLabel, constraints);
         constraints.gridx = 1;
         subPanel.add(inputField, constraints);
         constraints.gridx = 2;
+
         subPanel.add(choseInput, constraints);
         constraints.gridx = 3;
+
         subPanel.add(chooseLabel, constraints);
         constraints.gridx = 4;
+
         subPanel.add(encodeRadioBtn, constraints);
         constraints.gridy = 1;
         constraints.gridx = 0;
+
         subPanel.add(destinationLabel, constraints);
         constraints.gridx = 1;
+
         subPanel.add(outputField, constraints);
         constraints.gridx = 2;
+
         subPanel.add(choseOutput, constraints);
         constraints.gridx = 3;
+
         subPanel.add(ciphersCombo, constraints);
         constraints.gridx = 4;
+
         subPanel.add(decodeRadioBtn, constraints);
         return subPanel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if ("exit".equalsIgnoreCase(e.getActionCommand())) ;
+        System.exit(0);
     }
 }
